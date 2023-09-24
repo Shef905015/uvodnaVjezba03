a886e1e3ac79a6a1a9d54a4e2546bdf69ec3f045
Non't <90344104+Shef905015@users.noreply.github.com>
2023-09-24 11:28:22 +0200

    Add files via upload

---
+++ uvodnaVjezba03/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uvodnaVjezba03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a, b, c;
+
+            Console.Write("Molim vas unesite prvu katetu pravokutnog trokuta:");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Molim vas unesite drugu katetu pravokutnog trokuta:");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Molim vas unesite hipotenuzu pravokutnog trokuta:");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            if(a > 0 && b > 0 && c > 0)
+            {
+                if((a*a) + (b*b) == c)
+                {
+                    Console.WriteLine("Trokut je pravokutan!");
+                }
+                if (a * a + b * b != c)
+                {
+                    Console.WriteLine("Trokut je nije pravokutan!");
+                }
+
+
+            }
+            else
+            {
+
+                Console.ReadKey();
+
+            }
+
+
+
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
