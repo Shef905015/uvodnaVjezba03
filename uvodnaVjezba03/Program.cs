810e14c7885a538fab6e92d423a31fbc953893a0
Non't <90344104+Shef905015@users.noreply.github.com>
2023-09-24 18:25:15 +0200

    Update Program.cs

--- uvodnaVjezba03/Program.cs
+++ uvodnaVjezba03/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,34 +13,46 @@ namespace uvodnaVjezba03
 
             int a, b, c;
 
-            Console.Write("Molim vas unesite prvu katetu pravokutnog trokuta:");
+            unosA: Console.Write("Molim vas unesite prvu katetu pravokutnog trokuta:");
             a = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Molim vas unesite drugu katetu pravokutnog trokuta:");
-            b = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write("Molim vas unesite hipotenuzu pravokutnog trokuta:");
-            c = Convert.ToInt32(Console.ReadLine());
-
-            if(a > 0 && b > 0 && c > 0)
+            if (a < 0)
             {
-                if((a*a) + (b*b) == c)
+                Console.WriteLine("Prva kateta je manja od nule. ");
+                goto unosA;
+            }
+
+            unosB: Console.Write("Molim vas unesite drugu katetu pravokutnog trokuta:");
+            b = Convert.ToInt32(Console.ReadLine());
+            if (b < 0)
+            {
+                Console.WriteLine("Druga kateta je manja od nule. ");
+                goto unosB;
+            }
+
+            unosC: Console.Write("Molim vas unesite hipotenuzu pravokutnog trokuta:");
+            c = Convert.ToInt32(Console.ReadLine());
+            if (c < 0)
+            {
+                Console.WriteLine("Hipotenuza je manja od nule. ");
+                goto unosC;
+            }
+
+
+
+
+
+            if (a * a + b * b == c * c)
                 {
                     Console.WriteLine("Trokut je pravokutan!");
                 }
-                if (a * a + b * b != c)
+                if (a * a + b * b != c * c)
                 {
                     Console.WriteLine("Trokut je nije pravokutan!");
                 }
 
 
-            }
-            else
-            {
-
-                Console.ReadKey();
-
-            }
+            
+           
 
 
 
